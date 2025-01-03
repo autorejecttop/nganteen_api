d4a5a444e3c2860865218d26a53f0b69b2a0e46e
autorejecttop <autorejecttop@outlook.com>
2025-01-03 15:58:13 +0700

    config: added database and env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -26,6 +28,22 @@ import { APP_GUARD } from '@nestjs/core';
         limit: 100,
       },
     ]),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: +configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        autoLoadEntities: true,
+        synchronize: configService.get('NODE_ENV') === 'dev',
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [
