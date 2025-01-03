58bd08ffd5c4ad8c828b2cda5548ddc6320e14bd
autorejecttop <autorejecttop@outlook.com>
2025-01-03 15:29:03 +0700

    config: added Helmet for protection, CSRF protection, rate limiter, and enabled CORS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    // This module enables throttling to limit the
+    // rate of requests sent by users.
+    // Read more: https://docs.nestjs.com/security/rate-limiting
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // Needed to enable the rate limiter
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyCsrfProtection from '@fastify/csrf-protection';
+import fastifyHelmet from '@fastify/helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      logger: true,
+    }),
+  );
+
+  // This enables Helmet, it can help protect against
+  // some well known web vulnerabilities by setting
+  // HTTP headers. The order of applied middlewares
+  // is important!
+  // Read more: https://docs.nestjs.com/security/helmet#use-with-fastify, https://helmetjs.github.io/
+  await app.register(fastifyHelmet);
+
+  // This enables CSRF/XSRF protection.
+  // Read more: https://docs.nestjs.com/security/csrf#use-with-fastify
+  await app.register(fastifyCsrfProtection);
+
+  // This enables CORS and allow every IP addresses.
+  // For better security, please try to limit them
+  // to only trusted IP addresses.
+  // Read more: https://docs.nestjs.com/security/cors
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
