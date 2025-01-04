457f685add90ed4b4face4e00612f73548b3e1a4
autorejecttop <autorejecttop@outlook.com>
2025-01-04 16:44:05 +0700

    feat: added entity and enum for product

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,37 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ProductType } from '../enums/product-type.enum';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({
+    type: 'decimal',
+  })
+  price: number;
+
+  @Column({
+    nullable: true,
+    type: 'float',
+  })
+  rating: number;
+
+  @Column({
+    type: 'enum',
+    enum: ProductType,
+  })
+  type: ProductType;
+
+  @Column({
+    type: 'text',
+  })
+  description: string;
+
+  // TODO: Many to one Seller
+  @Column()
+  seller: string;
+}
