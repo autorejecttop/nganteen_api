90a8a96af005a73493d896019a3be8af45c14c6c
autorejecttop <autorejecttop@outlook.com>
2025-01-12 17:47:30 +0700

    test: Added E2E testing to test authentication

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,106 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
+import { AuthModule } from '../src/auth/auth.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+// See: https://docs.nestjs.com/fundamentals/testing#end-to-end-testing
+// & https://fastify.dev/docs/latest/Guides/Testing/
+describe('App', () => {
+  let app: NestFastifyApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, AuthModule],
     }).compile();
+    app = moduleRef.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
 
-    app = moduleFixture.createNestApplication();
     await app.init();
+    await app.getHttpAdapter().getInstance().ready();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('should return 401 when not logged in', () => {
+    it('/ (GET)', async () => {
+      const response = await app.inject({
+        method: 'GET',
+        url: '/',
+      });
+      expect(response.statusMessage).toBe('Unauthorized');
+      expect(response.statusCode).toBe(401);
+    });
+
+    it('/users (GET)', async () => {
+      const response = await app.inject({
+        method: 'GET',
+        url: '/user',
+      });
+      expect(response.statusCode).toBe(401);
+    });
+
+    it('/products (GET)', async () => {
+      const response = await app.inject({
+        method: 'GET',
+        url: '/product',
+      });
+      expect(response.statusCode).toBe(401);
+    });
+  });
+
+  describe('should not return 401 when logged in', () => {
+    let token;
+
+    beforeAll(async () => {
+      token = (
+        await app.inject({
+          method: 'POST',
+          url: '/auth/login',
+          payload: {
+            email: 'Franco_Paucek93@example.com',
+            password: '12345aA@',
+          },
+        })
+      ).json().access_token;
+    });
+
+    it('/ (GET)', async () => {
+      const response = await app.inject({
+        method: 'GET',
+        url: '/',
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+      expect(response.statusCode).toBe(200);
+    });
+
+    it('/users (GET)', async () => {
+      const response = await app.inject({
+        method: 'GET',
+        url: '/user',
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+      expect(response.statusCode).toBe(200);
+    });
+
+    it('/products (GET)', async () => {
+      const response = await app.inject({
+        method: 'GET',
+        url: '/product',
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+      expect(response.statusCode).toBe(200);
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
