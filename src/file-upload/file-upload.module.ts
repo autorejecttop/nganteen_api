0613caad91a35e42d157958b776d337c981a499e
autorejecttop <autorejecttop@outlook.com>
2025-01-12 17:34:18 +0700

    fix: Removed unused file-upload controller

--- src/file-upload/file-upload.module.ts
+++ src/file-upload/file-upload.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FileUploadService } from './file-upload.service';
-import { FileUploadController } from './file-upload.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Photo } from './entities/photo.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Photo])],
   providers: [FileUploadService],
-  controllers: [FileUploadController],
   exports: [FileUploadService],
 })
 export class FileUploadModule {}
